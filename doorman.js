91ca449b58dad312e9514acc4d712f929bd549cc
Simon Bugert <simon.bugert@googlemail.com>
2016-06-13 20:07:28 +0000

    Add ability to pass true to index option

--- doorman.js
+++ doorman.js
@@ -2,9 +2,9 @@ const http = require('http');
 const st = require('st');
 const auth = require('http-auth');
 
-var stMount = st({
+const stMount = st({
   path: process.env.DIRECTORY,
-  index: process.env.INDEX,
+  index: process.env.INDEX === 'true' ? true : process.env.INDEX,
   cache: process.env.CACHE === 'true'
 });
 
@@ -14,7 +14,6 @@ const basic = auth.basic({ realm: process.env.REALM },
   }
 );
 
-// Creating new HTTP server. 
 http.createServer(basic, function(req, res) {
   stMount(req, res);
 }).listen(process.env.PORT);
